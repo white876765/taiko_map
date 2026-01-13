57f789f4165bb02edbd58c95960174be38af63e9
white876765 <white876765@gmail.com>
2026-01-13 12:12:34 +0900

    Update map.js

--- map.js
+++ map.js
@@ -138,7 +138,7 @@ document.getElementById("toggleControls").onclick = () => {
 };
 
 // ===== JSON 読み込み =====
-fetch("shops_japan_all.json")
+fetch("data/shops.json")
   .then(r => r.json())
   .then(data => {
     originalShops = data.shops;
@@ -182,6 +182,7 @@ fetch("diff.json")
 
     renderMap();
   });
+
 
 
 
